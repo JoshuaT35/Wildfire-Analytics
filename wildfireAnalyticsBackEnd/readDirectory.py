3bccd3b0c9e4ea31021dc47e84b9ad11a6020092
Joshua Tablan <joshuatablan03@gmail.com>
2023-10-12 13:10:31 -0600

    cleaned up and added blocks to calc. area burned each year and graph this area

--- wildfireAnalyticsBackEnd/readDirectory.py
+++ wildfireAnalyticsBackEnd/readDirectory.py
@@ -17,7 +17,7 @@ Notes:
 For pure functionality: should shapefileName be an array?
 Should it return value error instead of any empty string?
 '''
-def getShapeFile(directoryName):
+def get_shape_file(directoryName):
     shp_files_list = glob.glob(f"{directoryName}/*.shp")
     if shp_files_list:
         shapefileName = shp_files_list[0]  # directoryName/shapefileName.shp
